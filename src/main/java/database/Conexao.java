d3efe7c7a3e89e2f5d1d307b166ed8f18374ac4b
Gustavo Campos <gustavo.gamer03@gmail.com>
2025-05-24 18:36:00 -0300

    Começando inserção de dados do CSV no bando de dados

--- src/main/java/database/Conexao.java
+++ src/main/java/database/Conexao.java
@@ -3,8 +3,8 @@ import java.sql.*;
 
 public class Conexao {
     private static final String URL = "jdbc:mysql://localhost:3306/projeto_golpes";
-    private static final String USER = "root";
-    private static final String PASSWORD = "0000";
+    private static final String USER = "gustavinho";
+    private static final String PASSWORD = "123456";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
