aee941144924d6afcdddf9ef95ad9e5e38cb593a
victoroscar30 <victoroscar30@gmail.com>
2025-06-10 22:48:13 -0300

    Sites de Segurança Site

--- src/main/java/ui/ImagemAvisos/AvisoVisualFactory.java
+++ src/main/java/ui/ImagemAvisos/AvisoVisualFactory.java
@@ -10,37 +10,37 @@ public class AvisoVisualFactory {
 
         lista.adicionar(new ImagemAviso(
                 "https://raw.githubusercontent.com/victoroscar30/avisos-imagens-publicas/main/Phishing_1.png",
-                "https://www.google.com/",
+                "https://www.kaspersky.com.br/resource-center/preemptive-safety/phishing-prevention-tips",
                 true
         ));
 
         lista.adicionar(new ImagemAviso(
                 "https://raw.githubusercontent.com/victoroscar30/avisos-imagens-publicas/main/Phishing_2.png",
-                "https://www.google.com/",
+                "https://wise.com/pt/help/articles/2960083/o-que-e-a-verificacao-em-duas-etapas",
                 true
         ));
 
         lista.adicionar(new ImagemAviso(
                 "https://raw.githubusercontent.com/victoroscar30/avisos-imagens-publicas/main/SitesDesconhecidos_1.png",
-                "https://www.google.com/",
+                "https://www.gov.br/pt-br/noticias/justica-e-seguranca/2021/08/fique-de-olho-para-nao-cair-em-golpes-virtuais",
                 true
         ));
 
         lista.adicionar(new ImagemAviso(
                 "https://raw.githubusercontent.com/victoroscar30/avisos-imagens-publicas/main/SitesDesconhecidos_2.png",
-                "https://www.google.com/",
+                "https://transparencyreport.google.com/safe-browsing/search",
                 true
         ));
 
         lista.adicionar(new ImagemAviso(
                 "https://raw.githubusercontent.com/victoroscar30/avisos-imagens-publicas/main/SitesSuspeitos_1.png",
-                "https://www.google.com/",
+                "https://www.spcbrasil.org.br/blog/como-saber-se-um-site-e-seguro",
                 true
         ));
 
         lista.adicionar(new ImagemAviso(
                 "https://raw.githubusercontent.com/victoroscar30/avisos-imagens-publicas/main/SitesSuspeitos_2.png",
-                "https://www.google.com/",
+                "https://www.serasa.com.br/premium/blog/pharming/",
                 true
         ));
 
